ebf498233652a8d2be56cff4282b02462bea31d6
liang <liang>
2015-04-28 17:05:38 +0800

    HTML target attirbute
    在新的页面打开链接

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,9 +1,20 @@
 import webapp2
 
+form=""""
+<form action="http://www.google.com/search" target="_blank">
+	<input name="q">
+	<!-- submit and enter is the same -->
+	<input type="submit" value="Goolge">
+</form>
+""""
+
 class MainPage(webapp2.RequestHandler):
 	def get(self):
+		# self.response.headers['Content-Type'] = 'text/plain'
+		# self.response.write('Hello, World!')
+		
 		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello, World!')
+		self.response.write(form)
 
 app = webapp2.WSGIApplication([('/', MainPage)], 
 						      debug=True)

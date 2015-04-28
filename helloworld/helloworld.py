9bc2e1002300aa7fcd0a66956967c6053e869927
liang <liang>
2015-04-28 19:48:32 +0800

    password: must use post method to keep safe

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -27,6 +27,13 @@ form2="""
 </form>
 """
 
+form3="""
+<form method="post" action="/password" >
+	<input type="password" name="q">
+	<input type="submit" value="password" formtarget="_blank">
+</form>
+"""
+
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		# self.response.headers['Content-Type'] = 'text/plain'
@@ -36,6 +43,7 @@ class MainPage(webapp2.RequestHandler):
 		self.response.write(form)
 		self.response.write(form1)
 		self.response.write(form2)
+		self.response.write(form3)
 		
 class TestHandler(webapp2.RequestHandler):
 	def get(self):
@@ -54,9 +62,20 @@ class TestHandler2(webapp2.RequestHandler):
 		self.response.headers['Content-Type'] = 'text/plain'
 		self.response.out.write(self.request) #[GET]: "get" 将参数加进了URL, POST将参数加进 request body 中
 		
+class TestHandler3(webapp2.RequestHandler):
+	def post(self):
+		# q=self.request.get("q")
+		# self.response.out.write(q+'<br>')  
+		
+		# [GET]:display http response
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.out.write(self.request) 
+		
   		
 		
 app = webapp2.WSGIApplication([('/', MainPage),
 								('/testform', TestHandler),  # [GET]:add a new handler 
-								('/testform2', TestHandler2)],
+								('/testform2', TestHandler2),
+								('/password', TestHandler3)
+								],
 						      debug=True)   

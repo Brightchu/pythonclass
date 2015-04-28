f90e623b94ded4f9a4f9e643a1c05f791bc6ad13
liang <liang>
2015-04-28 17:22:44 +0800

    python to interpret HTML

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,19 +1,18 @@
 import webapp2
 
-form=""""
-<form action="http://www.google.com/search" target="_blank">
+form="""
+<form action="http://www.bing.com/search" >
 	<input name="q">
 	<!-- submit and enter is the same -->
-	<input type="submit" value="Goolge">
 </form>
-""""
+"""
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		# self.response.headers['Content-Type'] = 'text/plain'
 		# self.response.write('Hello, World!')
 		
-		self.response.headers['Content-Type'] = 'text/plain'
+		#self.response.headers['Content-Type'] = 'text/plain'
 		self.response.write(form)
 
 app = webapp2.WSGIApplication([('/', MainPage)], 

684805ece6885251029e6f53075469054cee20ec
liang <liang>
2015-04-28 19:10:11 +0800

    Lesson 'more handler'

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,21 +1,41 @@
+# -*- coding: utf-8 -*- 
 import webapp2
 
-form="""
+# [GET]:添加最上面这一行就能支持中文了
+
+form1="""
 <form action="http://www.bing.com/search" >
 	<input name="q">
 	<!-- submit and enter is the same -->
 	<input type="submit" value="Bing" formtarget="_blank">
 </form>
 """
+form2="""
+<form action="/testform" >
+	<input name="q">
+	<!-- submit and enter is the same -->
+	<input type="submit" value="Testform2" formtarget="_blank">
+</form>
+"""
+
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		# self.response.headers['Content-Type'] = 'text/plain'
-		# self.response.write('Hello, World!')
+		# self.response.write('测试')
 		
 		#self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write(form)
-
-app = webapp2.WSGIApplication([('/', MainPage)], 
-						      debug=True)
-
+		self.response.write(form2)
+		
+class TestHandler(webapp2.RequestHandler):
+	def get(self):
+		# q=self.request.get("q")
+		# self.response.out.write(q+'<br>')  
+		
+		# [GET]:display http response
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.out.write(self.request)                  
+		
+app = webapp2.WSGIApplication([('/', MainPage),
+								('/testform', TestHandler)],  # [GET]:add a new handler 
+						      debug=True)   

b87035859134afe19ed32caa7020691fde2869b9
liang <liang>
2015-06-03 16:38:34 +0800

    ...

--- templatelesson/templates.py
+++ templatelesson/templates.py
@@ -1,13 +1,22 @@
 import os
 import webapp2
 
-class Handler(webapp2.RequestHandler):
+form_html="""
+<form>
+<h2>Add a food</h2>
+<input type="text" name=food></input>
+<input type="hidden" name=food value="eggs"></input>
+<button>Add</button>
+</form>
+"""
+
+class Handler(webapp2.RequestHandler):    #define a new cal
 	def write(self,*a,**kw):
 		self.response.out.write(*a,**kw)
 
 class MainPage(Handler):
 	def get(self):
-		self.write("Hello, world!")
+		self.write(form_html)
 
 
 app=webapp2.WSGIApplication([('/',MainPage)
